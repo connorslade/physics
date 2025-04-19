aa403f3a468ef1f9b329e50cff68110c11a0d272
Connor Slade <connor@connorcode.com>
2025-04-19 08:08:24 -0400

    Start on shape matching

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 #![feature(get_many_mut)]
 
+use std::f32::consts::TAU;
+
 use engine::{
     application::{Application, ApplicationArgs},
     color::Rgb,
-    drawable::shape::{circle::Circle, line::Line},
+    drawable::shape::{
+        circle::Circle,
+        line::{Line, LineCap},
+    },
     exports::{nalgebra::Vector2, winit::window::WindowAttributes},
     graphics_context::{Anchor, Drawable},
 };
 use itertools::Itertools;
 
 struct Point {
+    initial: Vector2<f32>,
     position: Vector2<f32>,
     velocity: Vector2<f32>,
 }
@@ -29,36 +35,36 @@ fn main() {
         window_attributes: WindowAttributes::default(),
         asset_constructor: Box::new(|_constructor| {}),
         resumed: Box::new(|| {
-            let mut soft_bodies = vec![SoftBody {
-                points: vec![
-                    Point {
-                        position: Vector2::new(0.0, 0.0),
-                        velocity: Vector2::repeat(0.0),
-                    },
-                    Point {
-                        position: Vector2::new(0.0, 100.0),
-                        velocity: Vector2::repeat(0.0),
-                    },
-                    Point {
-                        position: Vector2::new(100.0, 100.0),
-                        velocity: Vector2::repeat(0.0),
-                    },
-                    Point {
-                        position: Vector2::new(100.0, 0.0),
-                        velocity: Vector2::repeat(0.0),
-                    },
-                ],
-                constraints: vec![
-                    Constraint {
-                        points: [0, 2],
-                        distance: 212.0,
-                    },
-                    Constraint {
-                        points: [1, 3],
-                        distance: 212.0,
-                    },
-                ],
-            }];
+            let mut soft_bodies = vec![];
+
+            soft_bodies.clear();
+
+            let mut points = Vec::new();
+            let mut constraints = Vec::new();
+
+            let n = 8;
+            let r = (150.0 * n as f32) / TAU;
+
+            for i in 0..n {
+                let t = i as f32 / n as f32 * TAU;
+                let pos = Vector2::new(t.cos(), t.sin()) * r;
+                points.push(Point {
+                    initial: pos,
+                    position: pos,
+                    velocity: Vector2::zeros(),
+                });
+
+                let j = (i + n / 2) % n;
+                constraints.push(Constraint {
+                    points: [i, j],
+                    distance: r * 2.0,
+                });
+            }
+
+            soft_bodies.push(SoftBody {
+                points,
+                constraints,
+            });
 
             Box::new(move |ctx| {
                 ctx.background(Rgb::repeat(0.235));
@@ -100,6 +106,56 @@ fn main() {
                     for point in body.points.iter_mut() {
                         point.velocity += Vector2::y() * -30.0 * ctx.delta_time;
                         point.position += point.velocity * ctx.delta_time;
+
+                        let floor = -center.y + 12.0;
+                        if point.position.y < floor {
+                            point.position.y = floor;
+                            point.velocity.y = 0.0;
+                        }
+                    }
+
+                    {
+                        let mut com = Vector2::zeros();
+                        let mut staring_com = Vector2::zeros();
+                        for point in body.points.iter() {
+                            com += point.position;
+                            staring_com += point.initial;
+                        }
+                        com /= body.points.len() as f32;
+                        staring_com /= body.points.len() as f32;
+
+                        Circle::new(8.0)
+                            .color(Rgb::hex(0x0000FF))
+                            .position(com + center, Anchor::Center)
+                            .z_index(1)
+                            .draw(ctx);
+
+                        for point in body.points.iter_mut() {
+                            let pos = point.initial - staring_com + com;
+                            distance_constraint(
+                                [
+                                    point,
+                                    &mut Point {
+                                        initial: Vector2::zeros(),
+                                        position: pos,
+                                        velocity: Vector2::zeros(),
+                                    },
+                                ],
+                                0.0,
+                                ctx.delta_time,
+                            );
+                            Circle::new(4.0)
+                                .color(Rgb::hex(0x00FF00))
+                                .position(pos + center, Anchor::Center)
+                                .z_index(1)
+                                .draw(ctx);
+                            Line::new(pos + center, point.position + center)
+                                .color(Rgb::hex(0xFF00))
+                                .thickness(4.0)
+                                .cap(LineCap::Round)
+                                .z_index(1)
+                                .draw(ctx);
+                        }
                     }
 
                     for (a, b) in body
@@ -124,6 +180,9 @@ fn main() {
                             .draw(ctx);
                     }
                 }
+                Line::new(Vector2::zeros(), Vector2::x() * ctx.size().x)
+                    .thickness(8.0)
+                    .draw(ctx);
             })
         }),
         multisample: Some(4),
